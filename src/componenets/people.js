9c29f67f34b9e01a5e146653b2108b23322c74d9
tunkhine126 <tunkhine126@gmail.com>
2019-09-18 17:42:44 -0400

    second commit

---
+++ src/componenets/people.js
@@ -0,0 +1,15 @@
+import React, {Component } from 'react'
+
+class People extends Component {
+
+
+
+
+  render() {
+    return (
+      <div>HELLO WORLD</div>
+    )
+  }
+}
+
+export default People;
