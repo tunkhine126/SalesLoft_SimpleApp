b29a4d804e0472824fbd36bd6fdec55aa377817a
tunkhine126 <tunkhine126@gmail.com>
2019-11-13 14:30:53 -0500

    Loader added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import People from './components/people'
 import FrequencyTable from './components/frequencyTable'
 import { Button } from 'react-bootstrap'
+import ReactLoading from 'react-loading';
+
 
 
 const url = 'https://api.salesloft.com/v2/people'
@@ -15,6 +17,8 @@ class App extends Component {
   state = {
     allPeople: [],
     buttonToggle: false,
+    loading: undefined,
+    displyFrequencyTable: false,
   }
 
   // fetching SalesLoft API 
@@ -27,7 +31,10 @@ class App extends Component {
         }
       })
       .then(res => res.json())
-      .then(res => this.setState({ allPeople: res })
+      .then(res => this.setState({ 
+        allPeople: res, 
+        loading: true 
+      })
       )
     }
 
@@ -35,16 +42,55 @@ class App extends Component {
     displayFrequency = () => {
       this.setState(prevState => ({buttonToggle: !prevState.buttonToggle}))
     }
+
+    // displayFrequencyButton = () => {
+    //   return(
+    //     <div className="button" id="button">
+    //       <Button onClick={() => this.displayFrequency()} variant="secondary" size="sm" className="button">Display Frequency Count</Button>
+    //     </div>
+    //   )
+    // }
+
+    // displayFrequencyTable = () => {
+    //   return (
+    //     <People allPeople={this.state.allPeople} />
+    //   )
+    // }
+
+    // getTables = () => {
+    //   if(this.state.loading)
+    //     return (
+    //       <div>
+    //         <ReactLoading className="loading" type="spokes" color="#2897D3" width="15%" />
+    //       </div>
+    //     )
+    //   else
+    //     return (
+    //       <div>
+    //         {this.displayFrequencyButton()}
+    //         {this.displayFrequencyTable()}
+    //       </div>
+    //     )
+    // }
   
   render() {
     return (
       <div className="App">
-        <img className="banner" src='https://1d9jx8pre4-flywheel.netdna-ssl.com/wp-content/uploads/2019/05/brand-page-tabs_logo-1.png' alt="Logo" />
-          <div className="button" id="button">
-          <Button onClick={() => this.displayFrequency()} variant="secondary" size="sm" className="button">Display Frequency Count</Button>
-           </div><br />
-          {this.state.buttonToggle ? <FrequencyTable allPeople={this.state.allPeople}/> : null}
-        <People allPeople={this.state.allPeople}/>
+        {!this.state.loading ? (
+          <div >
+            <ReactLoading className="loader" type={"bars"} color={"grey"} />
+            Loading!
+          </div>
+        ) : (
+          <div>
+            <img className="banner" src='https://1d9jx8pre4-flywheel.netdna-ssl.com/wp-content/uploads/2019/05/brand-page-tabs_logo-1.png' alt="Logo" />
+              {<div className="button" id="button">
+                <Button onClick={() => this.displayFrequency()} variant="secondary" size="sm" className="button">Display Frequency Count</Button>
+              </div>} <br />
+                {this.state.buttonToggle ? <FrequencyTable allPeople={this.state.allPeople} /> : null}
+              <People allPeople={this.state.allPeople} />
+          </div>
+         )}
       </div>
     );
   }
